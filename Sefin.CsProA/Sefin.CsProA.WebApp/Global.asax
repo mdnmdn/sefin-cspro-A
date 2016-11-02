b6ec49fc3eaadb81457cabf892ea151c908c8b0f
Marco De Nittis <marco.denittis@gmail.com>
2016-11-02 11:35:42 +0100

    draft webproject

---
+++ Sefin.CsProA/Sefin.CsProA.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sefin.CsProA.WebApp.MvcApplication" Language="C#" %>
---
+++ Sefin.CsProA/Sefin.CsProA.WebApp/Libs/GlobalConfiguration.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Sefin.CsProA.WebApp.Libs
+{
+    public class GlobalConfiguration
+    {
+        static GlobalConfiguration _instance = new GlobalConfiguration();
+
+        public static GlobalConfiguration Instance { get { return _instance; } }
+
+        private GlobalConfiguration()
+        {
+            this.Environment = (ConfigurationManager.AppSettings["Environment"] ?? "debug").ToLower();
+
+            StartupDate = DateTime.Now;
+
+            var asm = System.Reflection.Assembly.GetExecutingAssembly();
+            Version = asm.GetName().Version.ToString();
+            var asmFile = new System.IO.FileInfo(asm.Location);
+
+
+            if (asmFile.Exists)
+            {
+                BuildDate = asmFile.LastWriteTime;
+            }
+
+            //    this.DB = ParseConnectionString();
+        }
+
+        private static string ParseConnectionString()
+        {
+            var db = "-";
+            try
+            {
+                var conn = ConfigurationManager.ConnectionStrings["DataConnection"];
+                if (conn != null)
+                {
+                    var connString = conn.ConnectionString.ToLower();
+                    int start = connString.IndexOf("database=");
+                    if (start > 0)
+                    {
+                        connString = connString.Substring(start + "database=".Length);
+                        var end = connString.IndexOf(";");
+                        if (end > 0) db = connString.Substring(0, end);
+                        else db = connString;
+                    }
+                }
+            }
+            catch { }
+            return db;
+        }
+
+
+        public string Version { get; private set; }
+        public DateTime BuildDate { get; private set; }
+        public DateTime StartupDate { get; private set; }
+
+        public string Environment { get; private set; }
+
+        public string DB { get; private set; }
+    }
+}
